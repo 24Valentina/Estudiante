94897ebaa35096564ae9fbba286fd74227511b07
24Valentina <karenv.valbuenar@uqvirtual.edu.co>
2023-08-25 09:16:31 -0500

    se agrega el modelo del estudiante con los getter y setter

---
+++ src/main/java/co/edu/uniquindio/estudiante/model/Estudiante.java
@@ -0,0 +1,68 @@
+package co.edu.uniquindio.estudiante.model;
+
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private String correo;
+    private int semestre;
+    private int nota1;
+    private int nota2;
+    private int nota3;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+
+    public int getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(int nota3) {
+        this.nota3 = nota3;
+    }
+
+}
