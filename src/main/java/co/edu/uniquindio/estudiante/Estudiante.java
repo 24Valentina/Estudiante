e2d51c302ebdcecf987a71336b58f27c2fb60673
24Valentina <karenv.valbuenar@uqvirtual.edu.co>
2023-08-25 14:06:44 -0500

    se crearon los 3 estudiantes, con su respectiva informacion

--- src/main/java/co/edu/uniquindio/estudiante/Estudiante.java
+++ src/main/java/co/edu/uniquindio/estudiante/Estudiante.java
@@ -1,13 +1,13 @@
-package co.edu.uniquindio.estudiante.model;
+package co.edu.uniquindio.estudiante;
 
 public class Estudiante {
     private String nombre;
     private int edad;
     private String correo;
     private int semestre;
-    private int nota1;
-    private int nota2;
-    private int nota3;
+    private double nota1;
+    private double nota2;
+    private double nota3;
 
     public String getNombre() {
         return nombre;
@@ -41,27 +41,37 @@ public class Estudiante {
         this.semestre = semestre;
     }
 
-    public int getNota1() {
+    public double getNota1() {
         return nota1;
     }
 
-    public void setNota1(int nota1) {
+    public void setNota1(double nota1) {
         this.nota1 = nota1;
     }
 
-    public int getNota2() {
+    public double getNota2() {
         return nota2;
     }
 
-    public void setNota2(int nota2) {
+    public Estudiante(String nombre, int edad, String correo, int semestre, double nota1, double nota2, double nota3) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.semestre = semestre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public void setNota2(double nota2) {
         this.nota2 = nota2;
     }
 
-    public int getNota3() {
+    public double getNota3() {
         return nota3;
     }
 
-    public void setNota3(int nota3) {
+    public void setNota3(double nota3) {
         this.nota3 = nota3;
     }
 
--- src/main/java/co/edu/uniquindio/estudiante/Main.java
+++ src/main/java/co/edu/uniquindio/estudiante/Main.java
@@ -2,6 +2,17 @@ package co.edu.uniquindio.estudiante;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Estudiante estudiante1= new Estudiante("Ana", 16, "Ana@gmail.com", 3,4.5,
+                2.7,3.0);
+        Estudiante estudiante2= new Estudiante("Gabriel", 20, "Gabriel20@gmail.com", 6,
+                4.0,3.4,1.7);
+        Estudiante estudiante3= new Estudiante("Luis", 27, "Luisma@gmail.com", 8,2.3,
+                5.0,4.4);
+
+
+
+
+
     }
+    
 }
